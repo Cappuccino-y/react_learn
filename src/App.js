0399bf7756255e29489b0bc6145059510dee7ac7
yydyy77 <472860802>
2022-09-16 23:34:42 +0800

    first modified

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+
+function  Hello(prop){
+  return (
+      <div id='hello'>
+        hello world, {prop.name}
+      </div>
+  )
+}
 
 function App() {
+    const [count,setCount]=React.useState(0)
+    function update(){
+        setCount(count+1)
+    }
   return (
+      <div>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,7 +33,19 @@ function App() {
         </a>
       </header>
     </div>
+          <Hello/>
+    <button onClick={update}>Click to count</button>
+          {count}
+      </div>
   );
 }
 
-export default App;
+
+export default function App_attach(){
+    return(
+        <div>
+            <App/>
+            <Hello name="lyy"/>
+        </div>
+    )
+};
